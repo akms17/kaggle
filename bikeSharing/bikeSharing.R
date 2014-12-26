d73c2c54cb434f5afd1aeb5bfa94e082f5fe3a62
akshay <akshay>
2014-12-26 00:31:54 +0000

    Parag Model

--- bikeSharing/bikeSharing.R
+++ bikeSharing/bikeSharing.R
@@ -2,6 +2,7 @@
 
 # Load random forest library
 library(randomForest)
+library(ggplot2)
 
 # load helper library
 source("helpers.R")
@@ -12,8 +13,17 @@ bike <- read.csv("data/train.csv")
 # Introduce extra columns based on datetime variables
 bikeAugment <- augmentDateTime(bike)
 
-# Get rid of datetime, registered and casual fields
-bikeClean <- bikeAugment[c(2:9,12:17)]
+# Get rid of datetime, keep (registered and casual fields)  for now also convert columns to factor ones.
+bikeClean <- bikeAugment[c(2:16)]
+
+# Based on prior knowledge we know hour is most influential factor, so we add mean count per hour as a feature
+#hc <- aggregate(bikeClean$count, by=list(bikeClean$hour), FUN=median)
+
+#bikeClean["hrmediancnt"] <- 0
+
+#`for (i in 0:nrow(hc)-1){
+#  bikeClean$hrmediancnt[bikeClean$hour == i] <- as.numeric(hc[hc$Group.1 == i,][[2]])
+#}
 
 ## 75% of the sample size
 smp_size <- floor(0.75 * nrow(bikeClean))
@@ -26,16 +36,46 @@ bikeTrain <- bikeClean[train_ind, ]
 bikeTest <- bikeClean[-train_ind, ]
 
 
-#Find the optimal number of variables to try and split on each node
-#bestmtry <- tuneRF(bikeTrain[-9],bikeTrain$count, ntreeTry=100, stepfactor=1.5, improve=0.02, trace=TRUE, plot=TRUE, dobest=FALSE)
+# Get rid of some useless features like season weekday
 
-# This states that mtry 13 was the optimal number of variables to consider.
+
+#Find the optimal number of variables to try and split on each node for prediciting registered
+reg_mtry <- tuneRF(bikeTrain[-c(9:11)], bikeTrain$registered, ntreeTry=100, stepfactor=1.5, improve=0.01, trace=TRUE, plot=FALSE, dobest=FALSE)
+minindex <- which.min(reg_mtry[,2])
 
 #Train a random forest model
-rf <- randomForest(count ~ . , data=bikeTrain, mtry=13, ntree=1000, keep.FOREST=TRUE, importance=TRUE, test=bikeTest)
+reg_rf <- randomForest(registered ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + weekday + year + month + hour , data=bikeTrain, mtry=reg_mtry[minindex], ntree=1000, keep.FOREST=TRUE, importance=TRUE, test=bikeTest)
+varImpPlot(reg_rf)
 
-#calculateRsq(bikeTrain, bikeTestSet, rf)
-RMSLE(bikeTest, rf)
+RMSLE(bikeTest, reg_rf)
+
+# Now predict casual
+# Find the optimal number of variables to try and split on each node for prediciting casual
+bestmtry <- tuneRF(bikeTrain[-c(9:11)],bikeTrain$casual, ntreeTry=100, stepfactor=1.5, improve=0.01, trace=TRUE, plot=FALSE, dobest=FALSE)
+minidx <- which.min(bestmtry[,2])
+
+cas_rf <- randomForest(casual ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + weekday + year + month + hour , data=bikeTrain, mtry=bestmtry[minidx], ntree=1000, keep.FOREST=TRUE, importance=TRUE, test=bikeTest)
+varImpPlot(cas_rf)
+
+RMSLE(bikeTest, cas_rf)
+
+# Now predicting count
+# Find the optimal number of variables to try and split on each node for prediciting count
+newTrain <- bikeTrain[-c(9:10)]
+newTrain["reg_p"] <- predict(reg_rf, newdata=bikeTrain)
+newTrain["cas_p"] <- predict(cas_rf, newdata=bikeTrain)
+
+newTest <- bikeTest[-c(9:10)]
+newTest["reg_p"] <- predict(reg_rf, newdata=bikeTest) 
+newTest["cas_p"] <- predict(cas_rf, newdata=bikeTest) 
+
+mtry <- tuneRF(newTrain[-9], newTrain$count, ntreeTry=100, stepfactor=1.5, improve=0.01, trace=TRUE, plot=FALSE, dobest=FALSE)
+minidx <- which.min(mtry[,2])
+rf <- randomForest(count ~ . , data=newTrain, mtry=mtry[minidx], ntree=5000, keep.FOREST=TRUE, importance=TRUE, test=newTest)
+varImpPlot(rf)
+
+calculateRsq(newTrain, newTest, rf)
+RMSLE(newTest, rf)
 
 # Load the test dataset
 bikeNew <- read.csv("data/test.csv")
@@ -44,7 +84,16 @@ bikeNew <- read.csv("data/test.csv")
 bikeNewAugment <- augmentDateTime(bikeNew)
 
 # Get rid of datetime field
-bikeNewClean <- bikeNewAugment[c(2:14)]
+bikeNewClean <- bikeNewAugment[c(2:13)]
+
+#bikeNewClean["hrmediancnt"] <- 0
+
+#for (i in 0:nrow(hc)-1){
+#  bikeNewClean$hrmediancnt[bikeNewClean$hour == i] <- as.numeric(hc[hc$Group.1 == i,][[2]])
+#}
+
+bikeNewClean["reg_p"] <- predict(reg_rf, newdata=bikeNewClean)
+bikeNewClean["cas_p"] <- predict(cas_rf, newdata=bikeNewClean)
 
 predictres <- predict(rf, newdata=bikeNewClean)
 
--- bikeSharing/helpers.R
+++ bikeSharing/helpers.R
@@ -9,11 +9,14 @@ calculateRsq <- function(train, test, model) {
 
 augmentDateTime <- function(dataset) {
     dt <-  as.POSIXlt(dataset$datetime, "%Y-%m-%d %H:%M:%S")
-    dataset["weekday"]      <- dt$wday
-    dataset["year"]         <- dt$year + 1900
-    dataset["month"]        <- dt$mon
-    dataset["hour"]         <- dt$hour
-    dataset["dayOfMonth"]   <- dt$mday    
+    dataset["weekday"]      <- as.factor(dt$wday)
+    dataset["year"]         <- as.factor(dt$year + 1900)
+    dataset["month"]        <- as.factor(dt$mon)
+    dataset["hour"]         <- as.factor(dt$hour)
+    dataset$weather         <- as.factor(dataset$weather)
+    dataset$holiday         <- as.factor(dataset$holiday)
+    dataset$workingday      <- as.factor(dataset$workingday)
+    dataset$season          <- as.factor(dataset$season)
     return(dataset)
 }
 
@@ -22,3 +25,12 @@ RMSLE <- function(test, model) {
     res <- sqrt((1/nrow(test) * sum((log(prediction + 1) - log(test$count + 1))^2)))
     res
 }
+
+
+#addFeatureFromColumn <- function(dataset, featureMap, featurename, FUN) {
+ # dataset[featurename] <- 0  
+  
+  #for (i in 0:nrow(hc)-1){
+  #  bikeClean$hrmediancnt[bikeClean$hour == i] <- as.numeric(hc[hc$Group.1 == i,][[2]])
+  #}
+#}
